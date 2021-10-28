c9d8e38cb4997e1b3af2e6d627f1e03061618c7a
eriktja <erik.jarem@gmail.com>
2021-10-28 15:53:54 +0200

    change is to ==
    github actions klagde på bruken av 'is' i tester

--- Leap_year/project/tests/test_is_leap_year.py
+++ Leap_year/project/tests/test_is_leap_year.py
@@ -2,24 +2,24 @@ from project.leap_year.is_leap_year import is_leap_year, is_one_hundred, is_four
 
 
 def test_is_4_leap_year():
-    assert is_leap_year(4) is True
+    assert is_leap_year(4) == True
 
 
 def test_is_400_leap_year():
-    assert is_leap_year(400) is True
+    assert is_leap_year(400) == True
 
 
 def test_is_2000_leap_year():
-    assert is_leap_year(2000) is True
+    assert is_leap_year(2000) == True
 
 
 def test_is_100_not_leap_year():
-    assert is_one_hundred(300) is True
+    assert is_one_hundred(300) == True
 
 
 def test_is_four_hundred_function_return_zero():
-    assert is_four_hundred(400) is 0
+    assert is_four_hundred(400) == 0
 
 
 def test_is_500_hundred_not_leap_year():
-    assert is_leap_year(500) is False
+    assert is_leap_year(500) == False
